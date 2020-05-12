98d8cf948d179a3ef7c58886579a45f97144e466
Dave <callremz@gmail.com>
2020-05-12 20:58:08 +0100

    chore: remove console statements

--- src/index.js
+++ src/index.js
@@ -22,15 +22,15 @@ const errorHandler = (error) => {
   if (error.syscall !== 'listen') {
     throw error;
   }
-  const address = server.address();
-  const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
+  // const address = server.address();
+  // const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
   switch (error.code) {
     case 'EACCES':
-      console.error(`${bind} requires elevated privileges.`);
+      // console.error(`${bind} requires elevated privileges.`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(`${bind} is already in use.`);
+      // console.error(`${bind} is already in use.`);
       process.exit(1);
       break;
     default:
@@ -39,10 +39,12 @@ const errorHandler = (error) => {
 };
 
 server.on('error', errorHandler);
+/*
 server.on('listening', () => {
   const address = server.address();
   const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
   console.log(`Listening on ${bind}`);
 });
+*/
 
 server.listen(port);
