7e16056fc89852711c38566da24a0735f246eda5
Dave <callremz@gmail.com>
2020-05-23 05:15:38 +0100

    bug: remove faulty "rootdir" from jest.js.config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   moduleFileExtensions: ['js', 'json'],
-  rootDir: '__tests__',
   testRegex: ['.spec.js$', '.test.js$'],
   coverageDirectory: './coverage',
-  testEnvironment: 'node',
+  testEnvironment: 'node'
 };
